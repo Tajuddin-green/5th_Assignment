03813be295fe0c9d793b1c8bcdf002434965654e
tajuddin-chowdhury <cmtajuddinchowdhury@gmail.com>
2025-03-01 22:36:48 +0600

    added page switch option

--- scripts/gate.js
+++
@@ -1,7 +0,0 @@
-// document.getElementById("discover-box").addEventListener("click",(e)=>{
-//     e.preventDefault();
-//     window.location.href = "../questionPage/question.html"
-//     })
-
-
-
--- scripts/gateBack.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById("back-btn").addEventListener("click",(e)=>{
-//     e.preventDefault();
-//     window.location.href = "../index.html"
-//     })
-    
