b44d49358073bf0262d58c3de963e60cfed00e0d
tajuddin-chowdhury <cmtajuddinchowdhury@gmail.com>
2025-03-01 19:00:55 +0600

    added activity log and Activity history clean

---
+++ scripts/activityWall.js
@@ -0,0 +1,4 @@
+document.getElementById("clear-history").addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.getElementById("activity-wall").innerHTML = "";
+})
--- scripts/cardButtonEvent.js
+++ scripts/cardButtonEvent.js
@@ -15,34 +15,49 @@
 
 // });
 
-function cardWork(cardId){
+function cardWork(cardHeading, cardBtnId) {
+  document.getElementById(cardBtnId).addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("Board updated Successfully");
+    let assignedNumber = parseInt(
+      document.getElementById("assigned-number").innerText
+    );
 
-    document.getElementById(cardId).addEventListener("click", (e) => {
-        e.preventDefault();
-        alert("Board updated Successfully");
-       let assignedNumber = parseInt(document.getElementById("assigned-number").innerText)
-        console.log(assignedNumber);
-        assignedNumber -= 1;
-        document.getElementById("assigned-number").innerText = assignedNumber;
-      
-        let completedTaskNumber = parseInt(document.getElementById("completed-task-number").innerText)
-        completedTaskNumber += 1;
-        document.getElementById("completed-task-number").innerText = completedTaskNumber;
-       const cardBtn =  document.getElementById(cardId)
-       cardBtn.classList.add("bg-slate-200")
-       cardBtn.setAttribute("disabled", "true")
+    assignedNumber -= 1;
+    document.getElementById("assigned-number").innerText = assignedNumber;
 
+    let completedTaskNumber = parseInt(
+      document.getElementById("completed-task-number").innerText
+    );
+    completedTaskNumber += 1;
+    document.getElementById("completed-task-number").innerText =
+      completedTaskNumber;
+    const cardBtn = document.getElementById(cardBtnId);
+    cardBtn.classList.add("bg-slate-200");
+    cardBtn.setAttribute("disabled", "true");
 
-       
-      
-      });
-      
+    let cardHeadingText = document.getElementById(cardHeading).innerText;
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+
+    let div = document.createElement("div");
+    div.innerHTML = `
+       <div class="bg-[#F4F7FF] p-3 rounded-lg mt-2 font-semibold">
+<p>You have Complete The Task ${cardHeadingText} at ${time}</p>
+</div>`;
+
+    document.getElementById("activity-wall").appendChild(div);
+  let childNumber =   document.getElementById("activity-wall").children.length
+ if(childNumber === 6){
+    alert("")
+ }
+  
+  });
 }
 
-
-cardWork("card-1-btn")
-cardWork("card-2-btn")
-cardWork("card-3-btn")
-cardWork("card-4-btn")
-cardWork("card-5-btn")
-cardWork("card-6-btn")
+cardWork("card-1-heading", "card-1-btn");
+cardWork("card-2-heading", "card-2-btn");
+cardWork("card-3-heading", "card-3-btn");
+cardWork("card-4-heading", "card-4-btn");
+cardWork("card-5-heading", "card-5-btn");
+cardWork("card-6-heading", "card-6-btn");
